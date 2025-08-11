bc8295bbaff9ffb1aa8a3e1927cb89f77d15ab2c
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-08-11 10:53:29 -0300

    first class

---
+++ Aula00/Basic logic.py
@@ -0,0 +1,7 @@
+if (10%2 == 0) and (2>1):
+    print("Condition is True")
+
+if ((2+2==4) and not (2+2==5) and (2*2==2+2)):
+    print("All conditions are True")
+
+
---
+++ Aula00/odd or even.py
@@ -0,0 +1,6 @@
+x = int(input("digite um numero: "))
+if (x%2 ==0):
+    print("O numero é par")
+else:
+    print("O numero é impar")
+print("Fim do programa")
